28d7d827047e3580b9da14d3aab33768569a4953
selenaysasmaz62-afk <selenaysasmaz62@gmail.com>
2026-09-12 00:24:13 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import asyncio
-from motor.ogrenme_motoru import KelimeOgrenmeMotoru
-
-async def main():
-    await KelimeOgrenmeMotoru().run()
-
-if __name__ == "__main__":
-    asyncio.run(main())
